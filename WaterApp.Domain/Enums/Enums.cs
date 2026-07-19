15ef6048a89d9d75a88c9692ba0e2e15a8184e8d
yazdanalam17-star <yazdanalam17@gmail.com>
2026-07-19 19:31:23 +0530

    Create Enums.cs

---
+++ WaterApp.Domain/Enums/Enums.cs
@@ -0,0 +1,40 @@
+namespace WaterApp.Domain.Enums;
+
+public enum UserRole
+{
+    Buyer = 0,
+    Seller = 1,
+    Admin = 2
+}
+
+public enum SellerStatus
+{
+    Pending = 0,
+    Approved = 1,
+    Rejected = 2,
+    Suspended = 3
+}
+
+public enum OrderStatus
+{
+    Placed = 0,
+    Confirmed = 1,
+    OutForDelivery = 2,
+    Delivered = 3,
+    Cancelled = 4
+}
+
+public enum PaymentMode
+{
+    Online = 0,
+    CashOnDelivery = 1
+}
+
+public enum PaymentStatus
+{
+    Pending = 0,
+    Success = 1,
+    Failed = 2,
+    Refunded = 3,
+    CollectedInCash = 4
+}
