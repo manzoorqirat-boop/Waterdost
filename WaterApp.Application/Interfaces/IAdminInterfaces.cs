c8b7cae9a7d4c7412abf6d76b5b0d0241179fc95
yazdanalam17-star <yazdanalam17@gmail.com>
2026-07-26 21:03:50 +0530

    Update IAdminInterfaces.cs

--- WaterApp.Application/Interfaces/IAdminInterfaces.cs
+++ WaterApp.Application/Interfaces/IAdminInterfaces.cs
@@ -5,4 +5,6 @@ namespace WaterApp.Application.Interfaces;
 public interface IAdminService
 {
     Task<AdminStatsResponse> GetStatsAsync();
+    Task<List<AdminSellerResponse>> GetSellersAsync(string? status);
+    Task<AdminSellerResponse> UpdateSellerStatusAsync(Guid sellerId, string status);
 }
